16009581a0c7a1e713671716f059e4bae4be32d9
RanadipShrestha <ranadipshrestha2061@gmail.com>
2025-02-19 11:15:51 +0545

    Solving the Debug = False issues

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -16,8 +16,6 @@ import os
 from decouple import Config
 
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = Config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = False   
 
 ALLOWED_HOSTS = ["*"]
 
@@ -124,12 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
